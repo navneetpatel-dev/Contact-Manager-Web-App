d0e91a7c36d20a137c514dec2179cecd19d2ebeb
navneetpatel-dev <navneetp22875@gmail.com>
2021-01-15 19:27:15 +0530

    Added context to react

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import ContactContext from "./../../context/contact/contactContext";
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  return <></>;
+};
+
+export default Contacts;
