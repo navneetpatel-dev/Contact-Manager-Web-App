51f6053a9a8618a5106a077af20a63b8cf4ff780
navneetpatel-dev <navneetp22875@gmail.com>
2021-01-16 14:39:06 +0530

    set alert context functionality

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from "./../../context/alert/alertContext";
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -16,7 +21,13 @@ const Register = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Register SUbmit");
+    if (name === "" || email === "" || password === "") {
+      setAlert(" Please enter all fields", "danger");
+    } else if (password !== password2) {
+      setAlert(" Passwords do not match", "danger");
+    } else {
+      console.log("Register SUbmit");
+    }
   };
 
   return (
@@ -33,6 +44,7 @@ const Register = () => {
             name="name"
             value={name}
             onChange={onChange}
+            required
           />
         </div>
         <div className="form-group">
@@ -43,6 +55,8 @@ const Register = () => {
             name="email"
             value={email}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -53,6 +67,8 @@ const Register = () => {
             name="password"
             value={password}
             onChange={onChange}
+            required
+            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -63,6 +79,7 @@ const Register = () => {
             name="password2"
             value={password2}
             onChange={onChange}
+            required
           />
         </div>
         <input
