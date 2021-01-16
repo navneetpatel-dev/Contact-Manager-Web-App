384d51c7581febba738c809f90c94d3f7608983d
navneetpatel-dev <navneetp22875@gmail.com>
2021-01-16 11:47:48 +0530

    setcurrent and clearcurrent functionality added

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -56,8 +56,14 @@ const ContactState = (props) => {
   };
 
   // Set Current Contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
 
   // Clear Current Contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
 
   // Update Contact
 
@@ -69,8 +75,11 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
         addContact,
         deleteContact,
+        setCurrent,
+        clearCurrent,
       }}
     >
       {props.children}
