ae1709f22220bb3a4f21c6f3accdcbeb15eb993e
navneetpatel-dev <navneetp22875@gmail.com>
2021-01-17 18:46:30 +0530

    Login functionality added

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -19,15 +19,9 @@ export const authReducer = (state, action) => {
         loading: false,
         user: action.payload,
       };
-    case AUTH_ERROR:
-      localStorage.setItem("token", action.payload.token);
-      return {
-        ...state,
-        ...action.payload,
-        isAuthenticated: true,
-        loading: false,
-      };
+
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem("token", action.payload.token);
       return {
         ...state,
@@ -36,12 +30,15 @@ export const authReducer = (state, action) => {
         loading: false,
       };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem("token");
       return {
         ...state,
         token: null,
         isAuthenticated: false,
         loading: false,
+        user: null,
         error: action.payload,
       };
     case CLEAR_ERRORS:
