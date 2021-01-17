a9d05520e7324543c71fc65fe03b14d915645597
navneetpatel-dev <navneetp22875@gmail.com>
2021-01-17 22:52:56 +0530

    error removed in require

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
-const path = requie("path");
+const path = require("path");
 
 const app = express();
 
